4f92716ee98c77e778e18444e3a99c005303af34
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 16:11:22 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var txt2 = "Blue, green, grey, white, or black; smooth, ruffled, or mountainous; that ocean is not silent."
+
+
+
+console.log(txt2.search(";"));
+console.log(txt2.search("green"));
+console.log(txt2.search("blue"));/* da wir blue in unserem text nicht haben  */
